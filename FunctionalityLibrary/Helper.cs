77e00778d45602f304424f03f2d410c9e61ee8c4
Iyanu Akins <sundax4u2@gmail.com>
2020-01-26 19:17:34 +0100

    Implemented register method helper class

--- FunctionalityLibrary/Helper.cs
+++ FunctionalityLibrary/Helper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SQLite;
+using System.Threading;
 
 namespace FunctionalityLibrary
 {
@@ -11,7 +12,7 @@ namespace FunctionalityLibrary
         {
             string connectionstring = ConfigurationManager.ConnectionStrings["SqliteConnectionString"].ConnectionString;
             return new SQLiteConnection(connectionstring);
-            
+
         }
 
         public static Product GetProduct(int productId)
@@ -27,13 +28,13 @@ namespace FunctionalityLibrary
                     if (reader.Read())
                     {
                         return new Product(
-                                            productId,
-                                            reader.GetString(1),
-                                            reader.GetString(2),
-                                            Convert.ToDouble(reader.GetFloat(3)),
-                                            reader.GetInt32(4),
-                                            Convert.ToDateTime(reader.GetString(5))
-                                            );
+                            productId,
+                            reader.GetString(1),
+                            reader.GetString(2),
+                            Convert.ToDouble(reader.GetFloat(3)),
+                            reader.GetInt32(4),
+                            Convert.ToDateTime(reader.GetString(5))
+                        );
                     }
                     else
                     {
@@ -49,5 +50,37 @@ namespace FunctionalityLibrary
             }
         }
 
+        public static void Register(string name, string email, string password)
+        {
+            using (SQLiteConnection connection = Helper.ConnectToDb())
+            {
+                string query =
+                    "INSERT INTO order VALUES(@name, @email, @type, @password, @date, @totalOrder)";
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                command.Parameters.AddWithValue("@name", name);
+                command.Parameters.AddWithValue("@email", email);
+                command.Parameters.AddWithValue("@type", "customer");
+                command.Parameters.AddWithValue("@password", password);
+                command.Parameters.AddWithValue("@date", DateTime.Now.ToString());
+                command.Parameters.AddWithValue("@totalOrder", 0);
+
+                try
+                {
+                    connection.Open();
+                    int row = command.ExecuteNonQuery();
+                    Console.WriteLine(row > 0
+                        ? "Registration successful, proceed to login"
+                        : "Registration failed, please retry");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
     }
 }
