db4d53c3e2d0253edc6a1ed1ad16a4c93f91c1b4
Iyanu Akins <sundax4u2@gmail.com>
2020-01-26 17:10:20 +0100

    Implemented customer checkout feature

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,48 +10,48 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            
-//            using (SQLiteConnection connection = Helper.ConnectToDb())
-//            {
-//                connection.Open();
-//                Console.WriteLine(connection.State);
-////            }
 
-//            Product newProduct = new Product(1, "orange", "Sweet", 5.556, 8, DateTime.Now);
-//            List<Product> prod = new List<Product>
-//            {
-//                newProduct
-//            };
-//            Cart currentCart = new Cart();
-//            currentCart.TotalPrice = newProduct.Price;
-//            currentCart.ProductsInCart = prod;
-//            foreach (var pro in currentCart.ProductsInCart)
-//            {
-//                Console.WriteLine(pro.Id);
-//                Console.WriteLine(pro.Name);
-//                Console.WriteLine(pro.Price);
-//            }
-//            Console.WriteLine($"Price: {currentCart.TotalPrice}");
-//            Console.WriteLine($"Length: {currentCart.ProductsInCart.Count}");
-//            prod = new List<Product>
-//            {
-//                new Product(89, "Mango", "Sweet", 6.556, 2, DateTime.Now)
-//            };
-//            Console.WriteLine();
-//            Console.WriteLine();
+            using (SQLiteConnection connection = Helper.ConnectToDb())
+            {
+                //connection.Open();
+                Console.WriteLine(connection.State);
+            }
 
-//            currentCart.TotalPrice = 2.0;
-//            currentCart.ProductsInCart = prod;
+            //            Product newProduct = new Product(1, "orange", "Sweet", 5.556, 8, DateTime.Now);
+            //            List<Product> prod = new List<Product>
+            //            {
+            //                newProduct
+            //            };
+            //            Cart currentCart = new Cart();
+            //            currentCart.TotalPrice = newProduct.Price;
+            //            currentCart.ProductsInCart = prod;
+            //            foreach (var pro in currentCart.ProductsInCart)
+            //            {
+            //                Console.WriteLine(pro.Id);
+            //                Console.WriteLine(pro.Name);
+            //                Console.WriteLine(pro.Price);
+            //            }
+            //            Console.WriteLine($"Price: {currentCart.TotalPrice}");
+            //            Console.WriteLine($"Length: {currentCart.ProductsInCart.Count}");
+            //            prod = new List<Product>
+            //            {
+            //                new Product(89, "Mango", "Sweet", 6.556, 2, DateTime.Now)
+            //            };
+            //            Console.WriteLine();
+            //            Console.WriteLine();
 
-//            Console.WriteLine("--------------------------------------------------");
-//            foreach (var pro in currentCart.ProductsInCart)
-//            {
-//                Console.WriteLine(pro.Id);
-//                Console.WriteLine(pro.Name);
-//                Console.WriteLine(pro.Price);
-//            }
-//            Console.WriteLine($"Price: {currentCart.TotalPrice}");
-//            Console.WriteLine($"Length: {currentCart.ProductsInCart.Count}");
+            //            currentCart.TotalPrice = 2.0;
+            //            currentCart.ProductsInCart = prod;
+
+            //            Console.WriteLine("--------------------------------------------------");
+            //            foreach (var pro in currentCart.ProductsInCart)
+            //            {
+            //                Console.WriteLine(pro.Id);
+            //                Console.WriteLine(pro.Name);
+            //                Console.WriteLine(pro.Price);
+            //            }
+            //            Console.WriteLine($"Price: {currentCart.TotalPrice}");
+            //            Console.WriteLine($"Length: {currentCart.ProductsInCart.Count}");
             //string query = "SELECT * FROM users WHERE userid =  1";
             //SQLiteCommand command = new SQLiteCommand(query, connection);
             //SQLiteDataReader reader = command.ExecuteReader();
--- FunctionalityLibrary/Customer.cs
+++ FunctionalityLibrary/Customer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace FunctionalityLibrary
 {
@@ -13,18 +14,118 @@ namespace FunctionalityLibrary
             TotalOrders = totalOrders;
         }
 
-        public Cart Cart { get; } = new Cart();
+        public Cart Cart { get; private set; } = new Cart();
         public int TotalOrders { get; private set; }
-
-        public void AddToCart(Product product)
+        public int AddToCart(Product product)
         {
-            List<Product> newProduct = new List<Product>
-            {
-                product
-            };
+            bool isInCart = false;
 
-            Cart.TotalPrice = product.Price;
-            Cart.ProductsInCart = newProduct;
+            if (Cart.ProductsInCart.Count > 0)
+            {
+                foreach (var prod in Cart.ProductsInCart)
+                {
+                    if (product.Id == prod.Id)
+                    {
+                        isInCart = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!isInCart)
+            {
+                List<Product> newProduct = new List<Product>
+                {
+                    product
+                };
+
+                Cart.TotalPrice = product.Price;
+                Cart.ProductsInCart = newProduct;
+            }
+            else
+            {
+                Console.WriteLine("Product already added to cart");
+            }
+            
+            return Cart.ProductsInCart.Count;
         }
+
+        public int RemoveFromCart(int index)
+        {
+            Cart.TotalPrice = Cart.ProductsInCart[index].Price * -1;
+            Cart.ProductsInCart.RemoveAt(index);
+            return Cart.ProductsInCart.Count;
+        }
+
+        public void ClearCart()
+        {
+            Cart = new Cart();
+        }
+
+        public void EditProfile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Checkout()
+        {
+            int productCount = Cart.ProductsInCart.Count;
+            if (productCount > 0)
+            {
+                using (SQLiteConnection connection = Helper.ConnectToDb())
+                {
+                    StringBuilder productStringBuilder = new StringBuilder();
+                    productStringBuilder.Append(Cart.ProductsInCart[0].Id);
+                    for (var i = 1; i <  productCount; i++)
+                    {
+                        productStringBuilder.Append($",{Cart.ProductsInCart[1].Id}");
+                    }
+
+                    string query =
+                        "INSERT INTO order VALUES(@totalProduct, @customer, @productsInOrder, @totalPrice, @orderDate)";
+                    SQLiteCommand command = new SQLiteCommand(query, connection);
+                    command.Parameters.AddWithValue("@totalProduct", productCount);
+                    command.Parameters.AddWithValue("@customer", Id);
+                    command.Parameters.AddWithValue("@productsInOrder", productStringBuilder.ToString());
+                    command.Parameters.AddWithValue("@totalPrice", Cart.TotalPrice);
+                    command.Parameters.AddWithValue("@orderDate", DateTime.Now.ToString());
+
+                    try
+                    {
+                        connection.Open();
+                        int row = command.ExecuteNonQuery();
+                        if (row > 0)
+                        {
+                            try
+                            {
+                                query = "UPDATE users SET totalorders = totalorders + 1";
+                                command = new SQLiteCommand(query, connection);
+                                row = command.ExecuteNonQuery();
+                                if (row > 0)
+                                {
+                                    Cart = new Cart();
+                                    Console.WriteLine("Your order has been processed");
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Gosh! Something went wrong, please try again");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                    
+                }
+            }
+            
+        }
+
     }
 }
