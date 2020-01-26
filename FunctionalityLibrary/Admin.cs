92d7f33625bf0d8c5f3b7afc1ef927b23188b7e8
Iyanu Akins <sundax4u2@gmail.com>
2020-01-26 23:30:36 +0100

    Implemented update product method in admin class

--- FunctionalityLibrary/Admin.cs
+++ FunctionalityLibrary/Admin.cs
@@ -7,10 +7,7 @@ namespace FunctionalityLibrary
     class Admin : User
     {
         public Admin(int id, string name, string email, DateTime dateRegistered) 
-            :base(id, name, email,dateRegistered )
-        {
-
-        }
+            :base(id, name, email,dateRegistered ){ }
 
         public bool AddProduct(string name, string description, double price, int units)
         {
@@ -35,9 +32,61 @@ namespace FunctionalityLibrary
                     Console.WriteLine(e);
                     return false;
                 }
-                finally
+            }
+        }
+
+        public bool UpdateProduct(int productId, string name = "", string description = "", double price = 0.0, int units = 0)
+        {
+            using (SQLiteConnection connection = Helper.ConnectToDb())
+            {
+                string query;
+                SQLiteCommand command;
+                if (name != "" && description != "" && price != 0.0 && units != 0)
                 {
-                    connection.Close();
+                    query = "UPDATE products SET productname = @name, productdescription = @description, price = @price, units = units + @units";
+                    command = new SQLiteCommand(query, connection);
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@description ", description);
+                    command.Parameters.AddWithValue("@price", price);
+                    command.Parameters.AddWithValue("@units", units);
+                }
+                else if(name != "" && description != "")
+                {
+                    query = "UPDATE products SET productname = @name, productdescription = @description";
+                    command = new SQLiteCommand(query, connection);
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@description ", description);
+                }
+                else if (price != 0.0 && units != 0)
+                {
+                    query = "UPDATE products SET price = @price, units = units + @units";
+                    command = new SQLiteCommand(query, connection);
+                    command.Parameters.AddWithValue("@price", price);
+                    command.Parameters.AddWithValue("@units", units);
+                }
+                else if (price != 0.0)
+                {
+                    query = "UPDATE products SET price = @price";
+                    command = new SQLiteCommand(query, connection);
+                    command.Parameters.AddWithValue("@price", price);
+                }
+                else
+                {
+                    query = "UPDATE products SET units = units + @units";
+                    command = new SQLiteCommand(query, connection);
+                    command.Parameters.AddWithValue("@units", units);
+                }
+
+                try
+                {
+                    connection.Open();
+                    int row = command.ExecuteNonQuery();
+                    return row > 0;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return false;
                 }
             }
         }
